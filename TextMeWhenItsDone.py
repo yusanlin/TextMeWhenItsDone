8c2c65e290c82c402bfed9d462113c5a881628da
Yusan Lin <yusan@psu.edu>
2014-08-24 16:28:49 -0400

    2nd commit

--- TextMeWhenItsDone.py
+++ TextMeWhenItsDone.py
@@ -3,9 +3,15 @@ TextMeWhenItsDone.py
 """
 import smtplib
 
+GMAIL = "gmail.com"
+
 def TextMe(email, password, phonenumber, carrier, sender = "YO"):
-    if email == gmail:
+    if email[email.index("@")+1:] == GMAIL:
         server = smtplib.SMTP("smtp.gmail.com", 587)
-    
+
+    if carrier == "ATT":
+        carrier = "@mms.att.net"
+        
+    server.starttls()
     server.login(email, password)
     server.sendmail(sender, phonenumber + carrier, "Hey your program is done")
